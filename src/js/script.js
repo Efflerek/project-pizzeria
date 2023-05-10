aab8bce2bad8fa32514a417f07923465239531b7
Efflerek <piotr.effler@gmail.com>
2023-05-10 20:32:45 +0100

    Fetch error - bugs

--- src/js/script.js
+++ src/js/script.js
@@ -367,26 +367,26 @@
   class Cart {
     constructor(element) {
       const thisCart = this;
-
+  
       thisCart.products = [];
       thisCart.getElements(element);
       console.log('new Cart', thisCart);
       thisCart.initActions();
     }
-
+  
     getElements(element) {
       const thisCart = this;
       thisCart.dom = {};
       thisCart.dom.wrapper = element;
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
-      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList)
-      thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber)
-      thisCart.dom.subtotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice)
-      thisCart.dom.deliveryFee = thisCart.dom.wrapper.querySelector(select.cart.deliveryFee)
-      thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelectorAll(select.cart.totalPrice)
-      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form)
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
+      thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
+      thisCart.dom.subtotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
+      thisCart.dom.deliveryFee = thisCart.dom.wrapper.querySelector(select.cart.deliveryFee);
+      thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelector(select.cart.totalPrice);
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
     }
-
+  
     initActions() {
       const thisCart = this;
       thisCart.dom.toggleTrigger.addEventListener('click', function () {
@@ -395,15 +395,15 @@
       thisCart.dom.productList.addEventListener('updated', function () {
         thisCart.update();
       });
-      thisCart.dom.productList.addEventListener('remove', function () {
+      thisCart.dom.productList.addEventListener('remove', function (event) {
         thisCart.remove(event.detail.cartProduct);
       });
-      thisCart.dom.form.addEventListener('submit', function (callback) {
-        callback.preventDefault();
+      thisCart.dom.form.addEventListener('submit', function (event) {
+        event.preventDefault();
         thisCart.sendOrder();
       });
     }
-
+  
     sendOrder() {
       const thisCart = this;
       const url = settings.db.url + '/' + settings.db.orders;
@@ -416,12 +416,12 @@
         deliveryFee: thisCart.dom.deliveryFee.innerHTML,
         products: [],
       };
-      console.log('payload', payload)
-
+      console.log('payload', payload);
+  
       for (let prod of thisCart.products) {
         payload.products.push(prod.getData());
       }
-
+  
       const options = {
         method: 'POST',
         headers: {
@@ -429,43 +429,44 @@
         },
         body: JSON.stringify(payload),
       };
-
+  
       fetch(url, options)
         .then(function (response) {
           return response.json();
-        }).then(function (parsedResponse) {
+        })
+        .then(function (parsedResponse) {
           console.log('parsedResponse', parsedResponse);
         });
     }
-
+  
     add(menuProduct) {
       const thisCart = this;
-
+  
       console.log('adding product', menuProduct);
-
+  
       const generatedHTML = templates.cartProduct(menuProduct);
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
       thisCart.dom.productList.appendChild(generatedDOM);
-
+  
       thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
       console.log('thisCart.products', thisCart.products);
-
+  
       thisCart.update();
     }
-
+  
     update() {
       const thisCart = this;
-
+  
       const deliveryFee = settings.cart.defaultDeliveryFee;
       thisCart.totalNumber = 0;
       thisCart.subtotalPrice = 0;
-
+  
       for (let product of thisCart.products) {
         thisCart.totalNumber += product.amount;
         thisCart.subtotalPrice += product.price;
       }
       thisCart.totalPrice = 0;
-      if (thisCart.subtotalPrice != 0) {
+      if (thisCart.subtotalPrice !== 0) {
         thisCart.totalPrice = thisCart.subtotalPrice + deliveryFee;
       } else {
         thisCart.totalPrice = 0;
