ae6185779d47de2f263fc254e6568904f7c2a055
Efflerek <piotr.effler@gmail.com>
2023-05-24 18:24:38 +0100

    Updated AmountWidget.js

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,70 +1,57 @@
-import {settings, select} from '../settings.js';
-class AmountWidget {
-    constructor(element) {
-      const thisWidget = this;
-      console.log('AmountWidget', thisWidget);
-      console.log('constructor arguments:', element);
-      thisWidget.getElements(element);
-      // Fix the method call by removing "thisWidget" before "this.input.value"
-      thisWidget.setValue(settings.amountWidget.defaultValue);
+import { settings, select } from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-      // Call the initActions method to initialize event listeners
-      thisWidget.initActions();
-    }
+class AmountWidget extends BaseWidget {
+  constructor(element){
+    super(element, settings.amountWidget.defaultValue); 
 
-    getElements(element) {
-      const thisWidget = this;
+    const thisWidget = this;
 
-      thisWidget.element = element;
-      // Add missing import statement for "select" variable
-      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-    }
-
-    setValue(value) {
-      const thisWidget = this;
-
-      const newValue = parseInt(value);
-
-      /* TODO: Add validation */
-
-      if (thisWidget.value !== newValue && !isNaN(newValue)) {
-        if (newValue >= settings.amountWidget.defaultMin) {
-          if (newValue <= settings.amountWidget.defaultMax) {
-            thisWidget.value = newValue;
-          }
-        }
-      }
-      thisWidget.announce();
-      //thisWidget.value = newValue;
-      thisWidget.input.value = thisWidget.value;
-
-    }
-
-    initActions() {
-      const thisWidget = this;
-      thisWidget.input.addEventListener('change', function () {
-        thisWidget.setValue(thisWidget.input.value);
-      });
-      thisWidget.linkDecrease.addEventListener('click', function (event) {
-        event.preventDefault();
-        thisWidget.setValue(thisWidget.value - 1);
-      });
-      thisWidget.linkIncrease.addEventListener('click', function (event) {
-        event.preventDefault();
-        thisWidget.setValue(thisWidget.value + 1);
-      });
-    }
-
-    announce() {
-      const thisWidget = this;
-
-      const event = new CustomEvent('updated', {
-        bubbles: true
-      });
-      thisWidget.element.dispatchEvent(event);
-    }
+    thisWidget.getElements(element);
+    thisWidget.initActions();
   }
 
-  export default AmountWidget;
+  getElements(){
+    const thisWidget = this;
+
+    //thisWidget.element = element;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
+  }
+
+
+  isValid(value){
+    return !isNaN(value)
+    && value >= settings.amountWidget.defaultMin 
+    && value <= settings.amountWidget.defaultMax;
+  }
+
+  renderValue(){
+    const thisWidget = this;
+
+    thisWidget.dom.input.value = thisWidget.value;
+  }
+
+
+  initActions(){
+    const thisWidget = this;
+
+    thisWidget.dom.input.addEventListener('change', function(){
+      thisWidget.value = thisWidget.dom.input.value;
+    });
+
+    thisWidget.dom.linkDecrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+
+    thisWidget.dom.linkIncrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
+    });
+      
+  }
+}
+
+export default AmountWidget;
