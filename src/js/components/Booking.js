7afb95a8123b11a866e4c2095ac36aba26990865
Efflerek <piotr.effler@gmail.com>
2023-05-24 18:15:18 +0100

    Updated booking.js - API pobieranie przefiltrowanych danych

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,29 +1,72 @@
-import {templates, select, settings, classNames} from '../settings.js';
+import { templates, select, settings, classNames } from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
 
-export class Booking{
-class Booking {
+export default class Booking {
   constructor(element) {
     const thisBooking = this;
-    thisBooking.render(reserveTable);
+    thisBooking.render(element);
     thisBooking.initWidgets();
-
+    thisBooking.getData();
   }
+
+  getData() {
+    const thisBooking = this;
+
+    const params = {
+      booking: [],
+      eventsCurrent: [],
+      eventsRepeat: [],
+    };
+
+    const urls = {
+      
+      booking:
+        settings.db.url +
+        '/' +
+        settings.db.booking +
+        '?' +
+        params.booking.join('&'),
+
+      eventsCurrent:
+        settings.db.url +
+        '/' +
+        settings.db.event +
+        '?' +
+        params.eventsCurrent.join('&'),
+
+      eventsRepeat:
+        settings.db.url +
+        '/' +
+        settings.db.event +
+        '?' +
+        params.eventsRepeat.join('&'),
+    };
+  }
+
   render(element) {
     const thisBooking = this;
     const generateBookingHTML = templates.bookingWidget();
     thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
     thisBooking.dom.wrapper.innerHTML = generateBookingHTML;
-    thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
-    thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
-    thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
-    thisBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
+    thisBooking.dom.peopleAmount = document.querySelector(
+      select.booking.peopleAmount
+    );
+    thisBooking.dom.hoursAmount = document.querySelector(
+      select.booking.hoursAmount
+    );
+    thisBooking.dom.datePicker = document.querySelector(
+      select.widgets.datePicker.wrapper
+    );
+    thisBooking.dom.hourPicker = document.querySelector(
+      select.widgets.hourPicker.wrapper
+    );
     thisBooking.dom.form = document.querySelector(select.booking.form);
   }
+
   initWidgets() {
     const thisBooking = this;
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
@@ -31,16 +74,21 @@ class Booking {
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
     thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
 
-    thisBooking.dom.wrapper.addEventListener('updated', function(){
+    thisBooking.dom.wrapper.addEventListener('updated', function () {
       thisBooking.updateDOM();
     });
 
-    thisBooking.dom.form.addEventListener('submit', function(event) {
+    thisBooking.dom.form.addEventListener('submit', function (event) {
       event.preventDefault();
       thisBooking.sendBooking();
     });
   }
-}
-}
 
-export default Booking;
+  updateDOM() {
+    // Code for updating the DOM based on changes in the widgets goes here
+  }
+
+  sendBooking() {
+    // Code for sending the booking data goes here
+  }
+}
