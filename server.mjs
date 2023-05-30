2c94f28e6dc989fd4b5e76ef30433f80f5d78799
Efflerek <piotr.effler@gmail.com>
2023-05-30 21:15:08 +0100

    server.mjs updated

--- server.mjs
+++ server.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import path from 'path';
 import jsonServer from 'json-server';
 
@@ -16,22 +15,3 @@ server.use(router);
 server.listen(port);
 
 export default server;
-=======
-import path from 'path';
-import jsonServer from 'json-server';
-
-const server = jsonServer.create();
-const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
-const middlewares = jsonServer.defaults({
-  static: 'dist',
-  noCors: true,
-});
-const port = process.env.PORT || 3131;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
-
-export default server;
->>>>>>> e095814da093e28a894f2e8ad3c3402a47fdc631
